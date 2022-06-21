7179ac6c8e583a0bc3d0c81f964791b6c0839f4d
SaurabhPik <saurabhsri.mca13@gmail.com>
2022-06-21 15:50:40 +0530

    threads 1st example modified

--- src/FirstExample.java
+++ src/FirstExample.java
@@ -3,13 +3,14 @@ public class FirstExample {
         hi obj1 = new hi();
         hello obj2 = new hello();
 
-        obj1.show();
-        obj2.show();
+        obj1.start();
+        try { Thread.sleep(10); } catch (Exception e) {}
+        obj2.start();
     }
 }
 
-class hi {
-    public void show() {
+class hi extends Thread {
+    public void run() {
         for (int i=1; i<=5; i++) {
             System.out.println("hi");
             try { Thread.sleep(1000); } catch (Exception e) {}
@@ -17,8 +18,8 @@ class hi {
     }
 }
 
-class hello {
-    public void show() {
+class hello extends Thread {
+    public void run() {
         for (int i=1; i<=5; i++) {
             System.out.println("hello");
             try { Thread.sleep(1000); } catch (Exception e) {}
