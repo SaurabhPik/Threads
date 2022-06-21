c103dbc0d3d991ed190b25634e6ba636fc24a42d
SaurabhPik <saurabhsri.mca13@gmail.com>
2022-06-21 15:49:30 +0530

    threads 1st example

--- src/FirstExample.java
+++ src/FirstExample.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class FirstExample {
+public class FirstExample {
+    public static void main(String[] args) {
+        hi obj1 = new hi();
+        hello obj2 = new hello();
+
+        obj1.show();
+        obj2.show();
+    }
 }
+
+class hi {
+    public void show() {
+        for (int i=1; i<=5; i++) {
+            System.out.println("hi");
+            try { Thread.sleep(1000); } catch (Exception e) {}
+        }
+    }
+}
+
+class hello {
+    public void show() {
+        for (int i=1; i<=5; i++) {
+            System.out.println("hello");
+            try { Thread.sleep(1000); } catch (Exception e) {}
+        }
+    }
+}
+
